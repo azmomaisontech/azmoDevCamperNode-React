3f1a998441ff9aaf5b25ed8bdc36750f752599f8
Moses Aizee <47111539+azmomaisontech@users.noreply.github.com>
2021-10-01 17:03:38 +0100

    optimize private route

--- client/src/util/PrivateRoute.tsx
+++ client/src/util/PrivateRoute.tsx
@@ -1,25 +1,29 @@
 import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, RouteProps } from "react-router-dom";
 import { AuthContext } from "../context/auth/AuthState";
 
-const PrivateRoute = ({ component: Component, ...rest }: any) => {
+interface Props extends RouteProps  {
+
+}
+
+const PrivateRoute = ({ component: Component, ...rest }: Props) => {
   const authContext = useContext(AuthContext);
   const { loading, isAuthenticated } = authContext;
+
+  if(!isAuthenticated && !loading) {
+    return <Redirect
+    to={{
+      pathname: "/login",
+      state: { from: props.location.pathname },
+    }}
+  />
+  }
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        !isAuthenticated && !loading ? (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: props.location.pathname },
-            }}
-          />
-        ) : (
           <Component {...props} />
-        )
       }
     />
   );
